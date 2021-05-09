5ac804bc00bd6e5fb8589b528c0f97591c23a8bf
Medoedushka <knife491@gmail.com>
2021-05-09 12:58:17 +0300

    Default pcb settings

--- PasswordsCollection/Model.cs
+++ PasswordsCollection/Model.cs
@@ -23,10 +23,7 @@ namespace PasswordsCollection
         const string PASSWORDS_PATH = "data.txt";
         string FullPasswordsFile;
 
-        public Model()
-        {
-
-        }
+        public Model() { }
 
         private void FillList()
         {
